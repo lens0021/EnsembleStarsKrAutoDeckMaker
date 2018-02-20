2464ecbd5d153300ef555970659e81c154ed5d75
lens0021 <lorentz0021@gmail.com>
2018-02-21 01:14:03 +0900

    쿠키

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var log = function( msg ) {
+	var showLog = true;
+	if ( showLog ) console.log( msg );
+}
+
 const abilityFromNumber = [ 'dance', 'vocal', 'performance' ];
 const typeFromNumber = abilityFromNumber;
 const typeToNumber = { dance: 0, vocal: 1, performance: 2 };
@@ -17,9 +22,9 @@ const minCardNum = 5;
 var data = undefined;
 
 var newData = function() {
-	var data = new Uint32Array(1+4*maxCardNumber);
+	var data = new Uint32Array(2+4*maxCardNumber);
 	data[1] = 5;
-	for ( var i=2; i < data.length; i+=4 )
+	for ( var i=2; i < 1+data[1]*4; i+=4 )
 		data[i] = ~0;
 	return data;
 }
@@ -35,7 +40,7 @@ var getData = function( request ) {
 		return data[1];
 	}
 	else {
-		if ( data[2+request*4]==~0 )
+		if ( data[2+request*4] == ~0 )
 			return undefined;
 		return {
 			idol: idols[data[2+request*4]],
@@ -62,8 +67,6 @@ var setData = function( type, val ) {
 			data[2+(type*4)+2] = val['vocal'];
 		if ( val['performance'] != undefined )
 			data[2+(type*4)+3] = val['performance'];
-	} else {
-		addCardData( val );
 	}
 
 	onChangedData();
@@ -71,51 +74,62 @@ var setData = function( type, val ) {
 
 var getHexStringData = function() {
 	var str = '';
-	for ( var i=0; i < 2+data[1]*4; i++ ) {
-		str += data[i].toString(16);
+	for ( var i=0; i < 1+data[1]*4; i++ ) {
+		str += ('00000000' + data[i].toString(16)).substr(-8, 8);
 	}
 
 	return str;
 }
 
 var convertHexStringToData = function( str ) {
-	var data = new Uint32Array(1+4*maxCardNumber);
+	var data = new Uint32Array(2+4*maxCardNumber);
 	var j=0;
-	for( var i=0; i+8< str.length; i++ ) {
-		data[j] = parseInt( str.substring(i, i+8), 16 );
-		j++;
+	for( var i=0; i+8 < str.length; i+=8 ) {
+		data[j++] = parseInt( str.substring(i, i+8), 16 );
 	}
 
 	return data;
 }
 
+var writeCookie = function () {
+	log('쿠키를 씁니다.');
+	var hexData = getHexStringData();
+	var d = new Date();
+	var exdays = 30;
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+
+	var cookie = 'data='+hexData+';expires=' + d.toUTCString() + ';';
+
+	log('다음과 같은 쿠키를 씁니다: '+cookie);
+	document.cookie = cookie;
+	log('쿠키가 다음과 같이 쓰였습니다: '+document.cookie);
+}
+
 var removeCardDataAt = function( i ) {
 	data[1]--;
-	for ( var mid = 2+i*4; mid < 2+data[1]*4; mid++ ) {
+	for ( var mid = 2+i*4; mid <= 2+(data[1]+1)*4; mid++ ) {
 		data[mid] = data[mid+4];
 	}
-	data[mid] = ~0;
 	onChangedData();
 }
 
 var addCardData = function( card ) {
-	data[1]++;
+	log('카드 추가(카드 수:'+data[1]+')');
 	if ( card === undefined ) {
+		log(data[1]);
 		data[2 + data[1]*4] = ~0;
+		log(data);
 	} else {
 		data[2 + data[1]*4] = card['idol'];
 		data[2 + data[1]*4+1] = card['dance'];
 		data[2 + data[1]*4+2] = card['vocal'];
 		data[2 + data[1]*4+3] = card['performance'];
 	}
+	data[1]++;
 	onChangedData();
 }
 
 var $cardProto;
-
-var addCard = function() {
-	addCardData();
-}
 
 var pickerIndex = undefined;
 var openIdolPicker = function( index ) {
@@ -125,18 +139,21 @@ var openIdolPicker = function( index ) {
 
 var initializeCard = function( $card ) {
 	var index = $card.prevAll().length;
+	log(index+'번 카드를 초기화합니다.');
 
 	$card.find('.remove-card').click( function( event ) {
 		event.preventDefault();
 		if ( getData('cardNum') <= 5 )
 			return;
 
+		log(index+'번 카드를 없앱니다.');
 		removeCardDataAt( index );
 	} );
 
 	$card.find('.card-thumb').click( function( event ) {
 		event.preventDefault();
 
+		log(index+'를 위한 picker를 띄웁니다.');
 		openIdolPicker( index );
 	} );
 
@@ -150,7 +167,7 @@ var initializeCard = function( $card ) {
 			 if ( index <= $("input[type='number']").length - 1 )
 			 	$("input[type='number']").eq(index).focus().select();
 			 else {
-			 	addCard();
+			 	addCardData();
 			 	$("input[type='number']").eq(index).focus().select();
 			 }
 		 }
@@ -158,19 +175,18 @@ var initializeCard = function( $card ) {
 	var cnt=0;
 	$card.find("input[type='number']").on( 'input', function(e) {
 		var type = abilityFromNumber[$(this).prevAll().length];
-		setData(index, { type: $(this).val() } );
+		var card = {};
+		card[type] = $(this).val();
+		setData( index, card );
 	});
 };
 
-var onChangedData = function () {
+var onChangedData = function ( needToWriteCookie = true ) {
 	// 계산
-	console.log('쿠키를 씁니다.');
-	var hexData = getHexStringData();
-	var d = new Date();
-	var exdays = 30;
-	d.setTime(d.getTime() + (exdays*24*60*60*1000));
-	document.cookie = 'data='+hexData+';expires=' + d.toUTCString() + ';';
-	console.log('쿠키가 다음과 같이 쓰였습니다: '+document.cookie);
+	// 쿠키 쓰기
+	if ( needToWriteCookie )
+		writeCookie();
+	
 	// 디자인 변경
 	// 속성
 	$('.deck')
@@ -182,16 +198,21 @@ var onChangedData = function () {
 	$('#unit2-switch').toggleClass( "active", getData('activeUnitNum') >= 2 );
 	$('#unit3-switch').toggleClass( "active", getData('activeUnitNum') >= 3 );
 
+	log( '카드가 ' + getData( 'cardNum' ) + '개 있습니다.');
+	var ct = 0;
 	while ( getData('cardNum') < $('.input .card').length ) {
 		$('.input .card:last').remove();
+		if ( ct++ > maxCardNumber*1.5 ) break;
 	}
+	ct = 0;
 	while ( getData('cardNum') > $('.input .card').length ) {
 		var clone = $cardProto.clone().insertBefore( $('.add-card') );
 		initializeCard( clone );
+		if ( ct++ > maxCardNumber*1.5 ) break;
 	}
 
 	$('.input .card').each( function( i ) {
-		$(this).find('.card-thumb').html( getData(i)['idol']);
+		$(this).find('.card-thumb').html( getData(i)['idol'] === undefined ? '' : getData(i)['idol'] );
 		$(this).find('.card-dance').attr('value', getData(i)['dance'])
 		$(this).find('.card-vocal').attr('value', getData(i)['vocal'])
 		$(this).find('.card-performance').attr('value', getData(i)['performance'])
@@ -202,34 +223,37 @@ var onChangedData = function () {
 
 $( document ).ready( function() {
 	$cardProto = $('.input .card').clone();
+	initializeCard($('.input .card'));
 
-	console.log( '쿠키가 있는지 확인합니다.' )
+	log( '쿠키가 있는지 확인합니다.' )
 	var cookie = document.cookie;
-	console.log( '쿠키는 다음과 같습니다: '+document.cookie);
+	// cookie = 'data=0022000000000007000000020000000a0000000a0000000a00000003000003f30000000f0000009a0000000400000073000000970000000fffffffff000005eb0000006f0000000e0000000d000000910000003d00000033ffffffff000002720000000f00000010ffffffff0000000000000000';
+	log( '쿠키는 다음과 같습니다: '+cookie);
 	var dataStr = cookie.match(/data=([^;]+)/);
 	if ( dataStr != null ) {
-		console.log( '쿠키에서 data를 가져왔습니다: '+dataStr[1]);
+		log( '쿠키에서 data를 가져왔습니다: '+dataStr[1]);
 		data = convertHexStringToData( dataStr[1] );
+		onChangedData( false );
 	}
 	else {
-		console.log( '쿠키에서 data를 가져오는데 실패하였습니다.');
+		log( '쿠키에서 data를 가져오는데 실패하였습니다.');
 		data = newData();
+		onChangedData();
 	}
-	onChangedData();
+	var ct =0;
 
 	for ( var i in idols ) {
 		var $idol = $('<li></li>').addClass( 'idol-picker-idol' ),
 		$anchor = $('<a></a>').attr( 'href','#' ).click( function( e ) {
 			var index = $(this).parent().prevAll().length;
 			event.preventDefault();
-			setData( pickerIndex, {idol: index} );
+			setData( pickerIndex, { idol: index } );
 			$('.overlay').toggle( false );
 		} ).html(idols[i]);
 		$idol.append( $anchor );
 		$idol.appendTo($('.idolPicker ol'));
+		if(ct++>50) break;
 	}
-
-	initializeCard($('.input .card'));
 
 	$('.switch').click( function(event) {
 		event.preventDefault();
@@ -266,7 +290,7 @@ $( document ).ready( function() {
 
 	$('.add-card a').click( function(event) {
 		event.preventDefault();
-		addCard();
+		addCardData();
 	});
 
 	$('.overlay').click( function(event) {
